6ce7da322582643ea5dd79809d6dbd67e8df7877
JohnPosada <johnposada2000@gmail.com>
2023-05-27 13:12:48 -0500

    added zoom

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,22 +1,26 @@
 import type { FC } from "react";
-import "../../box.css";
 import clsx from "clsx";
 interface Props {}
 const Box: FC<
   Props
 > = () => {
   const zoomStyle =
-    "hover:transition-all hover:duration-500 hover:ease-in-out hover:transform hover:bg-center hover:bg-cover";
+    "hover:scale-185 hover:ease-in-out";
 
   return (
     <>
-      <div className="card-zoom flex justify-center items-center text-3xl backdrop-blur-md w-full h-full text-white ">
+      <div className="flex justify-center items-center text-3xl backdrop-blur-md w-full h-full text-white ">
         <div
           className={clsx(
-            "h-2/4 w-2/4 rounded-2xl border-4 border-white shadow-blue-800 shadow-lg bg-cover bg-mybg bg-center",
-            zoomStyle
+            "h-2/4 w-2/4 overflow-hidden rounded-2xl border-4 border-white shadow-blue-800 shadow-lg"
           )}
         >
+          <div
+            className={clsx(
+              "w-full h-full bg-cover bg-mybg bg-center ",
+              zoomStyle
+            )}
+          ></div>
           my
           boiz
           000000000000000000000000000000
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-      backgroundImage: {'mybg': 'url(./src/assets/placeholder.jpg)'}
-    },
-	},
-	plugins: [],
-}
+module.exports =
+  {
+    content:
+      [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+      ],
+    theme:
+      {
+        extend:
+          {
+            backgroundImage:
+              {
+                mybg: "url(./src/assets/placeholder.jpg)",
+              },
+            scale:
+              {
+                175: "1.75",
+                185: "1.95",
+              },
+          },
+      },
+    plugins:
+      [],
+  };
