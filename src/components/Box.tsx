88d0f508571e007cb5c0f7122b8bc660fe546fdc
Alejandro Bernal <jbernalxyz@gmail.com>
2023-05-27 14:02:07 -0500

    added text over filter

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,31 +1,35 @@
 import type { FC } from "react";
 import clsx from "clsx";
 interface Props {}
-const Box: FC<
-  Props
-> = () => {
-  const zoomStyle =
-    "hover:scale-185 hover:ease-in-out";
+const Box: FC<Props> = () => {
+  const zoomStyle = "hover:scale-185 hover:ease-in-out";
 
   return (
     <>
+      {
+        //TODO: get this div to the layout
+      }
       <div className="flex justify-center items-center text-3xl backdrop-blur-md w-full h-full text-white ">
+        {
+          // BOX
+        }
         <div
-          className={clsx(
-            "h-2/4 w-2/4 overflow-hidden rounded-2xl border-4 border-white shadow-blue-800 shadow-lg"
-          )}
+          className={clsx("h-2/4 w-2/4 overflow-hidden rounded-2xl border-4 border-white shadow-blue-800 shadow-lg")}
         >
-          <div
-            className={clsx(
-              "w-full h-full bg-cover bg-mybg bg-center ",
-              zoomStyle
-            )}
-          >
-            <div className="bg-black w-full h-full opacity-70"></div>
+          {
+            // IMAGE
+          }
+          <div className={clsx("w-full h-full bg-cover bg-mybg bg-center ", zoomStyle)}>
+            {
+              // FILTER
+            }
+            <div className="w-full h-full grid grid-cols-2">
+              <span className="h-full w-full col-span-2 col-start-1 row-span-full z-10 flex justify-center items-center">
+                no sirve
+              </span>
+              <div className="h-full w-full bg-black opacity-70 col-span-2 col-end-3 row-span-full"> </div>
+            </div>
           </div>
-          my
-          boiz
-          000000000000000000000000000000
         </div>
       </div>
     </>
