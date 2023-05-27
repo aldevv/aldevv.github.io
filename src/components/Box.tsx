b7e304afe59f79ba1706717e605a40696b064066
Alejandro Bernal <jbernalxyz@gmail.com>
2023-05-27 11:39:06 -0500

    initial skel

---
+++ src/components/Box.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react";
+interface Props { }
+const Box: FC<
+  Props
+> = () => {
+  return (
+    <>
+      <div className="text-3xl backdrop-blur-md bg-blue-800 text-white h-full">
+        <div>
+          my
+          boiz
+          000000000000000000000000000000
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Box;
